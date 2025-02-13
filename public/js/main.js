cf8be8c52e538e84b38707b9869d6916881952a6
Kiamova Elvira <ela.kiyamova@yandex.ru>
2025-02-14 00:51:52 +0500

    feat: implement a video cover of the main page

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
+const videoElement = document.querySelector('.video-background');
 
+document.addEventListener('DOMContentLoaded', () => {    
+  if (navigator.connection) {
+    const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+    const effectiveType = connection.effectiveType;
+    
+    if (effectiveType === '4g') {
+      videoElement.play().catch(error => {
+        console.warn('Видео остановлено:', error);
+      });
+    } else {
+      videoElement.poster = "../images/main.jpg";
+      videoElement.load();
+    }
+  } else {
+    videoElement.play().catch(error => {
+      console.warn('Видео заблокировано:', error);
+    });
+  }
+});
